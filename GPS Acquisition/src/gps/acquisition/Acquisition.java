9fb73561ce155dd350c440c5866a85037150f230
Yue Wang <starlightwy@gmail.com>
2018-01-25 14:03:10 +0100

    first commit

---
+++ GPS Acquisition/src/gps/acquisition/Acquisition.java
@@ -0,0 +1,31 @@
+package gps.acquisition;
+
+
+public class Acquisition {
+	
+	public Acquisition(int nrOfSamples){
+		
+	}
+	
+	public void enterSample(float real, float imag){
+	}
+	
+	public void enterCode(float real, float imag){
+	}
+	
+	public boolean startAcquisition(){
+		
+		return false;
+	}
+	
+	public int getDopplerverschiebung(){
+		return 0;
+	}
+	
+	public int getCodeVerschiebung(){
+		return 0;
+	}
+	
+	
+
+}
